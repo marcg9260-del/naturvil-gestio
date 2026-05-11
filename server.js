24decb551b2504d2696562b79696a16080906e55
marcg9260-del <marcg9260@gmail.com>
2026-05-11 20:56:10 +0200

    server.js

--- server.js
+++ server.js
@@ -171,4 +171,4 @@ app.get('/api/factures', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Naturvil servidor actiu al port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Naturvil servidor actiu al port ${PORT}`));
